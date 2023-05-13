8f01d7f5fda64d56cd2e14ffde6296a82ccb4c25
221774 <22177@edu.fa.ru>
2023-05-13 14:51:37 +0300

    Test commit

--- services/server_221774.py
+++ services/server_221774.py
@@ -8,7 +8,7 @@ resnet = tf.keras.applications.ResNet101()
 with open('../data/imgnet_cats_ru.txt', encoding="utf-8") as f:
     cats = f.readlines()
 
-cats_ru = [s.rstrip() for s in cats]
+categories_ru = [s.rstrip() for s in cats]
 
 
 @app.route('/')
@@ -16,15 +16,15 @@ def home():
     return 'Home page'
 
 
-@app.route('/classify', methods = ['POST'])
+@app.route('/classify', methods = ['POST', 'GET'])
 def classify():
     data = request.data
     img = tf.io.decode_jpeg(data)
     img_t = tf.expand_dims(img, axis=0)
     img_t = tf.image.resize(img_t, (224, 224))
     out = resnet(img_t)
-    idxs = tf.argsort(out)[0][:3].numpy()
-    out = ', '.join([cats_ru[int(i)] for i in idxs])
+    idxs = tf.argsort(out, direction='DESCENDING')[0][:3].numpy()
+    out = ', '.join([categories_ru[int(i)] for i in idxs])
     return out
 
 if __name__ == '__main__':
--- services/server_221781.py
+++ services/server_221781.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
-app = Flask('Image classifier')
+app = Flask("Image classifier")
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Home page"
 
 
-if __name__ == '__main__':
-    app.run(port=1781)         # Your personal 4 digits
+if __name__ == "__main__":
+    app.run(port=1781)  # Your personal 4 digits
     input()
